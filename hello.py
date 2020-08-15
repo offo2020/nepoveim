652fe2579ea7d04a0540ff08338e27513b7703dc
Henrich Offermann <henrich.offermann@gmail.com>
2020-08-15 16:31:37 +0200

    poznamka do hello.py

---
+++ hello.py
@@ -0,0 +1,23 @@
+#ale predsa spravim zmenu
+nebran mi kamarat
+
+'''
+taky kodik ty pylint prisny
+'''
+
+def function():
+    '''
+    chce integer a da na druhu
+    '''
+
+    while True:
+        try:
+            num = int(input('Daj cele cislo chuju!: '))    
+        except:
+            print('to nebolo cele cislo, ze? Znova kamosko!')
+            continue
+        else:
+            break
+    print('jeho druha mocnina je: ', num**2)
+
+function()
